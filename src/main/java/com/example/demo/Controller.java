9b8fcfd58d52ecd34e6738e875864ec9e3f87fc8
User <User@DESKTOP-AB5H3TE>
2018-02-27 08:55:27 +0100

    Bean comunication simple 1

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  
@@ -17,4 +19,9 @@ public class Controller {
     public String home(){  
         return"nice home";  
     }  
+    
+    @RequestMapping( method=RequestMethod.GET)  
+    public @ResponseBody MyBean retunJson(){  
+        return new MyBean(12.2);  
+    } 
 }
---
+++ src/main/java/com/example/demo/MyBean.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class MyBean {
+	private final double amount;
+	
+	public MyBean(double amount) {
+		this.amount=amount;
+	}
+	
+	public double getAmount() {
+		return amount;
+	}
+}
